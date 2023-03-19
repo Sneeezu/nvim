16dda971a4ad2a6251360b2d7561377a3d2825d7
Sneeezu <sneeezu@proton.me>
2023-03-19 12:00:58 +0100

    Smoother notifications

--- lua/sneeeze/plugins/ui.lua
+++ lua/sneeeze/plugins/ui.lua
@@ -69,7 +69,13 @@ return {
 	{
 		"rcarriga/nvim-notify",
 		config = function()
-			vim.notify = require "notify"
+			local notify = require "notify"
+
+			notify.setup {
+				fps = 60,
+			}
+
+			vim.notify = notify
 		end,
 	},
 
