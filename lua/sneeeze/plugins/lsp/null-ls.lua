a69d889235c5808871b129360f0e33b3c03401e2
Sneeezu <sneeezu@proton.me>
2023-03-19 12:10:15 +0100

    Fix rustfmt to use correct edition

--- lua/sneeeze/plugins/lsp/null-ls.lua
+++ lua/sneeeze/plugins/lsp/null-ls.lua
@@ -15,7 +15,24 @@ return {
 				sources = {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.rustfmt,
+					null_ls.builtins.formatting.rustfmt.with {
+						extra_args = function(params)
+							local path = require "plenary.path"
+							local cargo_toml = path:new(params.root .. "/" .. "Cargo.toml")
+
+							-- Set default if Cargo.toml is not found
+							if not cargo_toml:exists() and not cargo_toml:is_file() then
+								return { "--edition=2021" }
+							end
+
+							for _, line in ipairs(cargo_toml:readlines()) do
+								local edition = line:match [[^edition%s*=%s*%"(%d+)%"]]
+								if edition then
+									return { "--edition=" .. edition }
+								end
+							end
+						end,
+					},
 				},
 			}
 		end,
