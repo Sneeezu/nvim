3221dc51e197b37d68f3ee1f6809a5c3ade6ac27
Sneeezu <sneeezu@proton.me>
2023-04-26 19:06:40 +0200

    Add nvim-treesitter-textobjects keymaps

--- lua/sneeeze/plugins/treesitter.lua
+++ lua/sneeeze/plugins/treesitter.lua
@@ -1,11 +1,13 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+
 		build = ":TSUpdate",
 		event = { "BufReadPost", "BufNewFile" },
 
 		dependencies = {
 			"romgrk/nvim-treesitter-context",
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 
 		config = function()
@@ -22,26 +24,43 @@ return {
 					enable = true,
 					enable_autocmd = false,
 				},
+
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+
+						goto_next_start = {
+							["]a"] = "@parameter.inner",
+							["]f"] = "@function.outer",
+							["]c"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]F"] = "@function.outer",
+							["]C"] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[a"] = "@parameter.inner",
+							["[f"] = "@function.outer",
+							["[c"] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[F"] = "@function.outer",
+							["[C"] = "@class.outer",
+						},
+					},
+				},
 			}
 		end,
 	},
 
 	{
 		"echasnovski/mini.ai",
-		event = "VeryLazy",
 
-		-- keys = {
-		-- 	{ "a", mode = { "x", "o" } },
-		-- 	{ "i", mode = { "x", "o" } },
-		-- },
+		event = { "BufEnter", "BufNewFile" },
 
 		dependencies = {
-			{
-				"nvim-treesitter/nvim-treesitter-textobjects",
-				init = function()
-					require("lazy.core.loader").disable_rtp_plugin "nvim-treesitter-textobjects"
-				end,
-			},
+			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
 
 		config = function()
@@ -51,12 +70,13 @@ return {
 				silent = true,
 
 				custom_textobjects = {
+					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+					a = ai.gen_spec.treesitter({ a = "@parameter.outer", i = "@parameter.inner" }, {}),
 					o = ai.gen_spec.treesitter({
 						a = { "@block.outer", "@conditional.outer", "@loop.outer" },
 						i = { "@block.inner", "@conditional.inner", "@loop.inner" },
 					}, {}),
-					f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
-					c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
 				},
 			}
 		end,
