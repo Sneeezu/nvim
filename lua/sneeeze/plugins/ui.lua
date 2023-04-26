39f9ebb0cc08534b50fe188236e3e0fdf8cfa2f4
Sneeezu <sneeezu@proton.me>
2023-04-26 20:56:57 +0200

    Move fidget.nvim config to lua/sneeeze/plugins/ui.lua

--- lua/sneeeze/plugins/ui.lua
+++ lua/sneeeze/plugins/ui.lua
@@ -181,5 +181,26 @@ return {
 		end,
 	},
 
+	{
+		"j-hui/fidget.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+
+		config = function()
+			require("fidget").setup {
+				text = {
+					spinner = "flip",
+				},
+
+				align = {
+					bottom = true,
+				},
+
+				window = {
+					relative = "editor",
+				},
+			}
+		end,
+	},
+
 	"stevearc/dressing.nvim",
 }
