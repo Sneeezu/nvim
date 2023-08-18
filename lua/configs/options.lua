6f0c5c9a6e1370be614620dd6aa5a403aa07d979
Sneeezu <sneeezu@proton.me>
2023-08-18 12:47:44 +0200

    Better formatting

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,53 +1,46 @@
 vim.g.mapleader = " "
 
-vim.opt.guicursor = ""
+vim.opt.conceallevel = 3
+vim.opt.guicursor = "" -- no cursor decorations
+vim.opt.laststatus = 3
+vim.opt.pumblend = 20 -- transparency
+vim.opt.showmode = false
+vim.opt.termguicolors = true
 
-vim.opt.fileencoding = "utf-8"
 vim.opt.spelllang = { "en", "cs" }
+vim.opt.updatetime = 25 -- 4000ms default
+vim.opt.completeopt = "menu,menuone,noinsert"
 
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.linebreak = true -- wraps lines smartly
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
+
+vim.opt.mouse = "a"
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 
 vim.opt.autoindent = true
 vim.opt.cindent = true
-
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
-
-vim.opt.wrap = false
-vim.opt.linebreak = true -- wraps lines smartly
-
-vim.opt.mouse = "a"
-
-vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.tabstop = 4
 
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
-
 vim.opt.smartcase = true
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.winminwidth = 5
 
-vim.opt.termguicolors = true
-vim.opt.pumblend = 20 -- transparency
-vim.opt.laststatus = 3
-vim.opt.showmode = false
-vim.opt.cursorline = true
-vim.opt.conceallevel = 3
-
-vim.opt.signcolumn = "yes"
-vim.opt.colorcolumn = "80"
-
-vim.opt.updatetime = 25 -- 4000ms default
-
-vim.opt.hidden = true
+vim.opt.fileencoding = "utf-8"
 vim.opt.swapfile = false
-
+vim.opt.hidden = true
 vim.opt.undofile = true
 vim.opt.undolevels = 1000
 vim.opt.undoreload = 10000
@@ -58,8 +51,6 @@ vim.opt.foldlevelstart = 99
 if vim.fn.executable "rg" == 1 then
 	vim.opt.grepprg = "rg --vimgrep"
 end
-
-vim.opt.completeopt = "menu,menuone,noinsert"
 
 vim.opt.list = true
 vim.opt.listchars:append {
--- lua/configs/plugins/utils.lua
+++ lua/configs/plugins/utils.lua
@@ -3,6 +3,7 @@ return {
 		"iamcco/markdown-preview.nvim",
 		ft = { "markdown" },
 		build = "cd app && npm install",
+
 		init = function()
 			vim.g.mkdp_filetypes = { "markdown" }
 		end,
